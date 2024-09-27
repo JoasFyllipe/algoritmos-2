43a1f59e387c9dc6b63e816071731b2290281aa8
Joás Fyllipe Vieira Solano <joassolano2908@gmail.com>
2024-09-27 16:47:11 -0300

    Questão 03

---
+++ Atividade_02/Questao_03/src/entities/Calculadora.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Calculadora {
+    public double num1;
+    public double num2;
+
+    public double somaNum(){
+        return num1+num2;
+    }
+    public double subtrairNum(){
+        return num1-num2;
+    }
+    public double multiplicarNum(){
+        return num1*num2;
+    }
+    public double divisaoNum(){
+        return num1/num2;
+    }
+
+}
